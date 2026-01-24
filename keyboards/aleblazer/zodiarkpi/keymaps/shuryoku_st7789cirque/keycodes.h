8cee88960f2ea7ac7f57dc9e1fcedbd69ddb1693
islathehut <ikoenigbu@gmail.com>
2026-01-24 16:32:45 -0500

    Add shuryoku keymap for ZodiarkPi with waveshare 2in display and cirque

---
+++ keyboards/aleblazer/zodiarkpi/keymaps/shuryoku_st7789cirque/keycodes.h
@@ -0,0 +1,21 @@
+#ifndef SHURYOKU_KEYCODE_H
+#define SHURYOKU_KEYCODE_H
+
+#include QMK_KEYBOARD_H
+
+// ┌───────────────────────────────────────────────────────────┐
+// │ d e f i n e   k e y c o d e s                             │
+// └───────────────────────────────────────────────────────────┘
+
+typedef enum {
+    U_CHG_OS = 256, // the standard seems to be SAFE_RANGE but that didn't compile - SAFE_RANGE ensures custom keycodes are outside the range 0-255
+    U_RDO,
+    U_PST,
+    U_CUT,
+    U_CPY,
+    U_UND,
+    U_SLCTALL,
+    U_SAVE
+} custom_keycodes;
+
+#endif
